3e5252c10ee3d3c83ed20d3da9d2229c95cc1751
harmy <hack4cn@gmail.com>
2022-09-09 18:23:23 +0800

    initial commit

---
+++ lib/rumbl/accounts.ex
@@ -0,0 +1,23 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+  alias Rumbl.Repo
+  alias Rumbl.Accounts.User
+
+  def list_users do
+    Repo.all(User)
+  end
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
+  end
+
+  def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
+end
---
+++ lib/rumbl/accounts/user.ex
@@ -0,0 +1,10 @@
+defmodule Rumbl.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    timestamps()
+  end
+end
